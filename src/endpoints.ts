320080c429ab4e16465dd64eb71ac68cc4bc2d40
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-09-01 20:56:23 +0530

    remove dependency dotenv

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,9 +1,9 @@
-const { APP_IDP_API_ENTRYPOINT, APP_IDP_VIEW_ENTRYPOINT } = process.env;
+import { IDP_API_ENTRYPOINT, IDP_VIEW_ENTRYPOINT } from '../config.json';
 
 export const endpoints = {
 	auth: {
-		refreshToken: `${APP_IDP_API_ENTRYPOINT}/auth/refresh-token`,
-		login: `${APP_IDP_VIEW_ENTRYPOINT}/login?redirect_uri=${window.location.href}`,
-		usersMe: `${APP_IDP_API_ENTRYPOINT}/users/me`,
+		refreshToken: `${IDP_API_ENTRYPOINT}/auth/refresh-token`,
+		login: `${IDP_VIEW_ENTRYPOINT}/login?redirect_uri=${window.location.href}`,
+		usersMe: `${IDP_API_ENTRYPOINT}/users/me`,
 	},
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config();
-
 export * from './endpoints';
 export * from './client';
 export * from './interceptors';
